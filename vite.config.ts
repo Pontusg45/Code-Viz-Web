9186041591f49b94321053b366ae35d85b2f54e4
Pontusg45 <62245536+Pontusg45@users.noreply.github.com>
2023-03-27 16:04:40 +0200

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 export default defineConfig({
   base: '/code-viz-web/',
-  plugins: [reactRefresh()]
+  plugins: [
+    reactRefresh(),
+    chunkSplitPlugin()
+  ]
 })
